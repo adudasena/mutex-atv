2cc54e7f5584bb856c01dd9609e35d41ff11ca82
Duda Sena <duda.sruiz@gmail.com>
2026-08-10 19:56:34 -0300

    feat: implementação da classe Trava

--- src/Trava.java
+++ src/Trava.java
@@ -1,2 +1,17 @@
 public class Trava {
+
+    // 1 com trava, 0 sem
+    private int trava= 0;
+
+    // mét que entra na região crítica
+    public void entrarNaCritica() {
+
+        while (true) {
+            if (trava == 0) {
+                trava = 1;
+                //trava a região crítica
+                return;
+            }
+        }
+    }
 }
